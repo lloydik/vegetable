4a1da3830c62d050fb5f5b3a7bd11ce722139d4b
Artem Glazyrin <artemglazyrin@mail.ru>
2020-05-31 16:42:38 +0300

    merge

--- app/src/main/java/com/example/appleandtomato/MainActivity.kt
+++ app/src/main/java/com/example/appleandtomato/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.appleandtomato
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a = 0
-        print(a)
-        //code branch
+        Timer("SettingUp", false).schedule(500) {
+            doSomething()
+        }
+    }
+    fun setOnClickListener(view: View){
+        var workSecs: Int = txwork.text.toString().toInt()
+        workSecs--
+        txtime.setText(workSecs.toString())
     }
 }
